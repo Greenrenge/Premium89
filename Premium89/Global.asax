1e750572933494cb0b460265fccaa5840a805bf5
green <green@GREENRENGE-PC>
2017-08-30 18:29:56 +0700

    Add project files.

---
+++ Premium89/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Premium89.MvcApplication" Language="C#" %>
---
+++ Premium89/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Premium89.Startup))]
+namespace Premium89
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
