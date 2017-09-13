e44fe450d868ef8fadf0b8fb61aa6db59d7e120a
green <green@GREENRENGE-PC>
2017-09-13 23:47:35 +0700

    add icon to Menu

---
+++ Premium89/Models/Themes/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Premium89.Models.Themes
+{
+    public class Menu
+    {
+        public Menu()
+        {
+            ChildMenus = new List<Menu>();
+        }
+        public string MenuName { get; set; }
+        public string Icon { get; set; }
+        public string DisplayText { get; set; }
+        public string Link { get; set; }
+        public short Order { get; set; }
+        public bool HasChild { get { return ChildMenus.Count > 0; } }
+        public List<Menu> ChildMenus { get; set; }
+    }
+}
